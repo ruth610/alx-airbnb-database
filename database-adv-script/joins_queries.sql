f66acd42a065b9de2cba7a2c3c2b70902198fb6b
ruth610 <ruthambaw610@gmail.com>
2025-10-28 15:29:53 +0300

    left join query improved

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,9 +2,9 @@ SELECT bookings.*, users.*
 FROM bookings 
 INNER JOIN users on bookings.user_id = users.user_id;
 
-SELECT p.*, r.*
-FROM properties p
-LEFT JOIN reviews r ON p.property_id = r.property_id;
+SELECT properties.*
+FROM properties
+LEFT JOIN reviews on properties.property_id = reviews.property_id;
 
 SELECT users.*, bookings.*
 FROM users
